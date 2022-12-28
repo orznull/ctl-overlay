76260907d534b1872953bf3ce6d3d886c7b560fb
orznull <tacothing@gmail.com>
2022-12-27 22:33:52 -0500

    Switches to hash router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import PlayerOverlay from './pages/PlayerOverlay';
 import PlayerOverlayConfig from './pages/PlayerOverlayConfig';
 const App = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/overlay" element={<PlayerOverlay />} />
         <Route path="/config" element={<PlayerOverlayConfig />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
